428dbd8af5d06596f591e35a514b0e143099a2df
Albert Llebaria Holda <Allebaria94@gmail.com>
2020-04-01 16:03:36 +0200

    -Modified UX of XML_Reader. Once a document has been uploaded it displays the content and the upload box is not displayed. The user now can review it, and send it or upload a new. If selects a new one then the upload box is displayed again

--- src/components/XML_Reader/index.jsx
+++ src/components/XML_Reader/index.jsx
@@ -35,13 +35,13 @@ const XMLSchemaReader = (props) => {
 
     return (
         <div className="container">
-            <form className="xml__reader" onSubmit={handleSubmit} >
+            {!props.schema && <form className="xml__reader" onSubmit={handleSubmit} >
                 <div className="xml__reader__input_container">
                     <label for="myfile">Upload an XML file to submit:</label>
                     <input type="file" id="myfile" name="xml_schema" onChange={e => setFile(e.target.files[0])}></input>
                 </div>
                 <input className="xml_form__submit" value="Upload" type="submit"></input>
-            </form>
+            </form>}
             <div className="xml__visualizer" style={{ height: props.schema ? '600px' : '0' }}>
                 {props.schema && (
                     <textarea disabled={true} className="xml__visualizer_content">
@@ -49,7 +49,11 @@ const XMLSchemaReader = (props) => {
                     </textarea>
                 )}
             </div>
-            {props.schema && <button className="xml_form__submit" >Approve</button>}
+            {props.schema && (<div className="xml__visualizer__comands">
+                <button className="xml_form__submit margin-10" onClick={() => props.setXMLSchema(null)} >New</button>
+                <button className="xml_form__submit margin-10" >Approve</button>
+            </div>)}
+
         </div>
     )
 }
