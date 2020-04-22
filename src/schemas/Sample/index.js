43a8fe6d70156a2299a4bc76b0917993399c5567
Albert Llebaria Holda <Allebaria94@gmail.com>
2020-04-22 10:58:53 +0200

    - Added missing fields from sample schema

--- src/schemas/Sample/index.js
+++ src/schemas/Sample/index.js
@@ -23,20 +23,33 @@ const tree = {
                 {
                     name: "SAMPLE_NAME",
                     content: false,
+                    required:true,
+                    meta:{
+                        display_name:{
+                            value:"",
+                            placeholder:"Display name"
+                        }
+                    },
                     childs: [
                         {
                             name: "TAXON_ID",
                             content: true,
+                            required:true,
+                            value:"",
                             placeholder: "PROVIDE NCBI TAXID FOR ORGANISM (e.g. 9606 for human)"
                         },
                         {
                             name: "SCIENTIFIC_NAME",
                             content: true,
+                            required:true,
+                            value:"",
                             placeholder: "SCIENTIFIC NAME AS APPEARS IN NCBI TAXONOMY FOR THE TAXON_ID (e.g. homo sapiens)"
                         },
                         {
                             name: "COMMON_NAME",
                             content: true,
+                            required:true,
+                            value:"",
                             placeholder: "OPTIONAL COMMON NAME AS APPEARS IN NCBI TAXONOMY FOR THE TAXON_ID (e.g. human)"
                         },
                     ]
@@ -45,6 +58,38 @@ const tree = {
                     name: "DESCRIPTION",
                     content: true,
                     placeholder: "A LONGER DESCRIPTION OF SAMPLE AND HOW IT DIFFERS FROM OTHER SAMPLES"
+                },
+                {
+                    name:"SAMPLE_LINKS",
+                    placeholder:"Links to resources related to this sample or sample set (publication, datasets, online databases).",
+                    content:false,
+                    multiple:true,
+                    childs:[
+                        {
+                            name:"SAMPLE_LINK",
+                            conent:false,
+                            childs: [
+                                {
+                                    name: "URL_LINK",
+                                    content: false,
+                                    childs: [
+                                        {
+                                            name: "LABEL",
+                                            content: true,
+                                            placeholder: "Data Access Agreement",
+                                            value: ""
+                                        },
+                                        {
+                                            name: "URL",
+                                            content: true,
+                                            placeholder: "Website address",
+                                            value: ""
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
                 },
                 {
                     name: "SAMPLE_ATTRIBUTES",
